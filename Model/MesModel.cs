d5324a730377c682300994955f12e2d0f4db4ae8
Jamil Salim Daruix <jamilsd@gmail.com>
2023-03-28 22:07:01 -0300

    Aula Dois

---
+++ Model/MesModel.cs
@@ -0,0 +1,22 @@
+using System.Reflection.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace meu_primiro_projeto_ef.Model
+{
+    [Table("Mes")]
+    public class MesModel
+    {
+        [Key]
+        [Column("CODIGO")]
+        public int Id { get; set; }    
+        
+        [MaxLength(100)]
+        public string Nome { get; set; }
+        public int Ano { get; set; }
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace meu_primiro_projeto_ef;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
